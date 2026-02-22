f159ab510b1ac83bd201bdd2b1a8857eaa46eda1
Luis Camilo <luis.camilo@ikigaidigital.io>
2026-02-22 20:47:34 +0000

    🐛 fixing bug when export to google

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation(project(":classification"))
 
     // Google Sheets API
-    implementation("com.google.api-client:google-api-client:2.0.0")
+    implementation("com.google.api-client:google-api-client:1.34.1")
     implementation("com.google.apis:google-api-services-sheets:v4-rev612-1.25.0")
     implementation("com.google.auth:google-auth-library-oauth2-http:1.16.0")
     implementation("com.google.http-client:google-http-client-jackson2:1.43.3")
